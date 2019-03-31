4f29b7fd43188db4f9696d1abfca5cd9da02879a
Nuno Lopes <nunolopes99@gmail.com>
2019-03-31 20:10:38 +0200

    User https

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
+require('dotenv').config()
+
 const fetch    = require('node-fetch');
 const RSS      = require('rss')
 const cheerio  = require('cheerio')
 const fs       = require('fs')
 
-const BASE_URL = 'http://tviplayer.iol.pt'
+async function scrape() {
 
-var feed = new RSS({
-    title: 'Governo Sombra',
-    feed_url: 'http://localhost/rss.xml',
-    site_url: BASE_URL,
-    image_url: 'http://www.iol.pt/multimedia/oratvi/multimedia/imagem/id/58a7505c0cf2b10cb6612529/',
-});
+    var feed = new RSS({
+        title: 'Governo Sombra',
+        feed_url: 'http://localhost/rss.xml',
+        site_url: process.env.BASE_URL,
+        image_url: 'https://www.iol.pt/multimedia/oratvi/multimedia/imagem/id/58a7505c0cf2b10cb6612529/',
+    });
 
-function scrape() {
-
-    fetch(`${BASE_URL}/programa/governo-sombra/53c6b3a33004dc006243d5fb`)
+    fetch(`${process.env.BASE_URL}/programa/governo-sombra/53c6b3a33004dc006243d5fb`)
         .then(data => data.text())
         .then(html => {
             const $ = cheerio.load(html)
@@ -22,17 +22,39 @@ function scrape() {
             $('#lista-episodios-content a').each(function(i, el) {
                 feed.item({
                     title: $(el).find('.item-title').text().trim().replace('Governo Sombra - ', ''),
-                    url: BASE_URL + $(el).attr('href'),
+                    url: process.env.BASE_URL + $(el).attr('href'),
                     date: $(el).find('.item-date').text(),
                 });
             })
 
             fs.writeFile("./rss.xml", feed.xml({ indent: true }), (err) => {
-                if (err) console.error(err)
+                if (err) throw new Error(err);
             })
-
         })
-        .catch(err => console.error(err))
 }
 
-module.export = scrape()
+async function run() {
+    try {
+        await scrape()
+    } catch (e) {
+        console.error(e.message)
+
+        if (process.env.ENV === 'live') {
+            await fetch(`https://api.pushover.net/1/messages.json`, {
+                method: 'post',
+                body: JSON.stringify({
+                    token: process.env.PUSHOVER_TOKEN,
+                    user: process.env.PUSHOVER_USER,
+                    title: "Scraper",
+                    message: e.message
+                }),
+                headers: { 'Content-Type': 'application/json' }
+            })
+            .then(res => res.json())
+            .then(json => console.log(json));
+        }
+    }
+}
+
+module.export = run()
+
